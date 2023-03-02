5657f0fdd092f8125903ad21767ae467db16b274
nelsenw <wnelsen9@gmail.com>
2023-03-02 15:13:52 -0800

    Expand vine class

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
+import Vine from "./scripts/vine"
+
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
     canvas.width = window.innerWidth 
     canvas.height = window.innerHeight
     
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = 'black'
-    
-    class Vine {
-        constructor(){
-            this.startPos = [Math.random() * canvas.width, Math.random() * canvas.height]
-            this.endPos = [Math.random() * canvas.width, Math.random() * canvas.height]
-        }
-        draw(){
-            ctx.beginPath();
-            console.log(this.startPos, this.endPos)
-            ctx.moveTo(this.startPos[0], this.startPos[1]);
-            ctx.lineTo(this.endPos[0], this.endPos[1]);
-            ctx.stroke();
-        }
+    const vines = [];
+    for( let i = 0; i < 10; i++){
+        vines.push(new Vine(canvas))
     }
-    
-    const vine1 = new Vine()
-    vine1.draw()
+    vines.forEach(vine => vine.draw(ctx))
 })
--- src/scripts/vine.js
+++ src/scripts/vine.js
@@ -1,15 +1,66 @@
 export default class Vine {
-    constructor(){
-        this.startPos = [Math.random() * canvas.width, Math.random() * canvas.height]
-        this.endPos = [Math.random() * canvas.width, Math.random() * canvas.height]
-        this.color = hsla(120, Math.random() * 100, Math.random() * 100)
+  constructor(canvas, ctx) {
+    this.ctx = ctx;
+    this.canvas = canvas;
+    this.startPos = [
+      Math.random() * this.canvas.width,
+      Math.random() * this.canvas.height,
+    ];
+    this.endPos = [
+      this.startPos[0] + (Math.random() - 0.5) * 800,
+      this.startPos[1] + (Math.random() - 0.5) * 800,
+    ];
+    this.h = 138;
+    this.s = 22;
+    this.l = 15;
+    this.color = `hsla(${this.h}, ${this.s}%, ${this.l}%)`;
+    this.random = (Math.random() - 0.5) * 800;
+    this.size = 9
+  }
+
+  shrink() {
+    this.h -= 4
+    this.s += 9
+    this.l += 14
+    if(this.size === 9){
+        this.size = 5
+    } else if(this.size === 5){
+        this.size = 3
+    } else if(this.size === 3){
+        this.size = 1
+    } else {
+        this.size = 0
     }
-    draw(){
-        ctx.strokeStyle = this.color
-        ctx.beginPath();
-        // console.log(this.startPos, this.endPos)
-        ctx.moveTo(this.startPos[0], this.startPos[1]);
-        ctx.lineTo(this.endPos[0], this.endPos[1]);
-        ctx.stroke();
+    this.color = `hsla(${this.h}, ${this.s}%, ${this.l}%)`;
+  }
+
+  draw(context) {
+    while(this.size > 0){
+        context.strokeStyle = this.color;
+        context.lineWidth = this.size;
+        context.beginPath();
+        context.moveTo(this.startPos[0], this.startPos[1]);
+        context.bezierCurveTo(
+          this.endPos[0] / 2,
+          this.endPos[1] / 2,
+          this.endPos[0] + this.random,
+          this.endPos[1] + this.random,
+          this.endPos[0],
+          this.endPos[1]
+        );
+        context.stroke();
+        this.shrink();
     }
-}y
+    this.vining();
+  }
+
+  
+
+  vining() {
+    this.startPos = this.endPos;
+    this.endPos = [
+      Math.random() * this.canvas.width,
+      Math.random() * this.canvas.height,
+    ];
+  }
+}
