fe637511cff8d5a3dbdf94e1c6a131498cdeb6ef
nelsenw <wnelsen9@gmail.com>
2023-03-27 15:07:16 -0700

    revamp vine class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import Vine from "./scripts/vine"
+import Vine from "./scripts/vine";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-    canvas.width = window.innerWidth 
-    canvas.height = window.innerHeight
-    
-    const vines = [];
-    for( let i = 0; i < 10; i++){
-        vines.push(new Vine(canvas))
-    }
-    vines.forEach(vine => vine.draw(ctx))
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+    const vine = new Vine(canvas, ctx)
+
+  const animate = () => {
+    // draw intial position
+    vine.draw(ctx, vine.dim, vine.color)
+    // grow vine
+    vine.grow()
+    window.requestAnimationFrame(animate)
+  };
+  animate();
+});
--- src/scripts/vine.js
+++ src/scripts/vine.js
@@ -4,38 +4,51 @@ export default class Vine {
     this.ctx = ctx;
     this.canvas = canvas;
     //generate a random spot for vine to start on outer edge of 100px
-    if(Math.random() > 0.5){
-        if(Math.random() < 0.5){
-            this.x = canvas.width
-        } else {
-            this.x = 0
-        }
-        this.y = canvas.height * Math.random()
-    } else {
-        if(Math.random() < 0.5){
-            this.y = canvas.height
-        } else {
-            this.y = 0
-        }
-        this.x = canvas.width * Math.random()
-    }
-
+    // if(Math.random() > 0.5){
+    //     if(Math.random() < 0.5){
+    //         this.x = canvas.width
+    //     } else {
+    //         this.x = 0
+    //     }
+    //     this.y = canvas.height * Math.random()
+    // } else {
+    //     if(Math.random() < 0.5){
+    //         this.y = canvas.height
+    //     } else {
+    //         this.y = 0
+    //     }
+    //     this.x = canvas.width * Math.random()
+    // }
+    this.x = 0
+    this.y = this.canvas.height
+    //starting width of the vine
     this.dim = 10
-    this.color = "green"
+    //starting color of the vine
+    this.h = 144;
+    this.s = 28;
+    this.l = 22;
+    this.color = `hsla(${this.h}, ${this.s}%, ${this.l}%)`
   }
 
-  draw(context) {
-    context.fillStyle = this.color
-    context.fillRect(this.x, this.y, this.dim, this.dim)
-    grow()
+  draw(context, dimension, color) {
+    context.fillStyle = color
+    context.fillRect(this.x, this.y, dimension, dimension)
+    this.shade(dimension)
+  }
+
+  shade(dim){
+    while(dim > 0){
+      dim -= 2
+      let mod = 10 - Math.abs(dim - 10)
+      this.draw(this.ctx, dim, `hsla(${this.h - mod}, ${this.s - mod}%, ${this.l - mod}%)`)
+    }
   }
 
   grow() {
       this.x += 1 
+      this.y -= 1
   }
 
-  animate(){
-    draw()
-    requestAnimationFrame(animate)
-  }
+
+
 }
