5f0690a683f4bea92bd989e5dbec7e917f5658c0
nelsenw <wnelsen9@gmail.com>
2023-04-14 16:22:30 -0700

    2nd transformation function

--- src/scripts/transform.js
+++ src/scripts/transform.js
@@ -14,10 +14,38 @@ export default function transform(currChar, nextChar, ctx, transformed) {
       }
     }
   }
-  ctx.putImageData(currChar, 100, 100);
-  setTimeout(() => {
-    if (!transformed) {
-      transform(currChar, nextChar, ctx, false);
+  // simple color shift from start to end
+
+  // pixels move outward
+
+  // create circle around center pixel
+
+  // blast outwards
+
+  for (let i = 0; i < currChar.data.length; i += 4) {
+    let j = Math.floor(i / 256);
+    let k = (i - j * 256) / 4;
+    let newPix;
+    if (j > 31 && k > 31) {
+      newPix = (j + 1) * 256 + (k + 1) * 4;
+    } else if (j > 31 && k <= 31) {
+      newPix = (j + 1) * 256 + (k - 1) * 4;
+    } else if (j <= 31 && k > 31) {
+      newPix = (j - 1) * 256 + (k + 1) * 4;
+    } else {
+        newPix = (j - 1) * 256 + (k - 1) * 4;
     }
-  }, 0);
+    currChar.data[newPix] = currChar.data[i]
+    currChar.data[newPix + 1] = currChar.data[i + 1]
+    currChar.data[newPix + 2] = currChar.data[i + 2]
+    currChar.data[newPix + 3] = currChar.data[i + 3]
+
+  }
+
+  ctx.putImageData(currChar, 100, 100);
+  //   setTimeout(() => {
+  //     if (!transformed) {
+  //       transform(currChar, nextChar, ctx, false);
+  //     }
+  //   }, 0);
 }
